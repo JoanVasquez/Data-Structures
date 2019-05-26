d322a67cba90e24a66d7c4cf8cedef1e687e6c0e
Joan Vasquez <yoloprogramo22@gmail.com>
2019-05-25 21:38:01 -0400

    Data Structure

---
+++ shell_sort.js
@@ -0,0 +1,26 @@
+let shellSort = () => {
+  let intArr = [20, 35, -15, 7, 55, 1, -22];
+  console.log(`Unsorted ===> ${intArr}`);
+
+  for (
+    let gap = Math.round(intArr.length / 2);
+    gap > 0;
+    Math.round((gap /= 2))
+  ) {
+    for (let i = gap; i < intArr.length; i++) {
+      let newElement = intArr[i];
+      let j = i;
+
+      while (j >= gap && intArr[j - gap] > newElement) {
+        intArr[j] = intArr[j - gap];
+        j -= gap;
+      }
+
+      intArr[j] = newElement;
+    }
+  }
+
+  console.log(`Sorted ===> ${intArr}`);
+};
+
+shellSort();
